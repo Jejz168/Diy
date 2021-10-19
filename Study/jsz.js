8629bddb3c2d88fdbd1786e20f4a07b2848792c8
hushenan <53441247+hushenan@users.noreply.github.com>
2021-10-19 17:27:50 +0800

    Update jsz.js

--- Study/jsz.js
+++ Study/jsz.js
@@ -204,10 +204,13 @@ async function task(){
         if(completeTodayCount >= 25){
             await taskSeq(1)
         }
-	    if(completeTodayCount >= 70){
+	    if(completeTodayCount >= 50){
             await taskSeq(2)
-            $.log("\n今日阅读${completeTodayCount}`篇已满 请明天再来")
-
+         }
+        if(completeTodayCount >= 70){
+            $.log("\n今日阅读70篇已满 请明天再来")
+           //$.log("\n=====开始提现=====")
+        return
         }else {
 		const CryptoJS = require('./crypto-js')
           var key = CryptoJS.enc.Utf8.parse("5kosc7jy2w0fxx3s")
